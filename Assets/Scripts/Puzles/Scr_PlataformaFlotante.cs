0a2063eaad83c3697e9be053011efdbcff3b19e0
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-05-17 09:46:59 +0200

    Cambios 2

---
+++ Assets/Scripts/Puzles/Scr_PlataformaFlotante.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_PlataformaFlotante : MonoBehaviour
+{
+    public bool move;
+	
+	void Update () {
+        move = Scr_MenhirPlatsFlotantes.activarPlataformas;
+    }
+}
