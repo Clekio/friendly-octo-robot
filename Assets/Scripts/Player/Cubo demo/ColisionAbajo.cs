57fceedcfd8311d900eaa93a287fef611226b664
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-05-18 02:29:20 +0200

    Cambios 1

---
+++ Assets/Scripts/Player/Cubo demo/ColisionAbajo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionAbajo : MonoBehaviour
+{
+    [SerializeField]
+    Animator anim;
+
+    public bool colisionAbajo = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("ColisionAbajo", colisionAbajo);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Ground")
+        {
+            colisionAbajo = true;
+        }
+    }
+}
