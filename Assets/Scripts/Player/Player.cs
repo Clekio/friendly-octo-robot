d186f7383e9a6a95b355947e12d579f6d2bc0551
Clekio <alvaro.ortuno.m@gmail.com>
2017-05-18 17:51:37 +0200

    Conseguido
    Implementado lo de Juan Raigada

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	public enum MovementMode {OnGround, OnAir, OnClimbing};
+	public MovementMode PlayerMode;
+
     public bool Death = false;
 
     public float maxHorizontalSpeed = 7;
@@ -18,6 +21,7 @@ public class Player : MonoBehaviour
 
     private bool grounded;
     const float k_GroundedRadius = .1f;                // Radius of the overlap circle to determine if grounded
+	[SerializeField]
     private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
 
     public float maxJumpHeight = 4;
@@ -26,151 +30,194 @@ public class Player : MonoBehaviour
 
     private SpriteRenderer spriteRenderer;
 	private Animator animator;
+	[SerializeField]
     private Rigidbody2D rb2d;
 
     private float m_mainGravityScale;
     private float m_glideGravityScale;
+	[SerializeField]
     float maxJumpVelocity;
+	[SerializeField]
     float minJumpVelocity;
     float velocityXSmoothing;
     float velocityYSmoothing;
 
+	public bool climbing;
+	public float ClimbingSpeed;
+
     // Use this for initialization
     void Start()
 	{
-		spriteRenderer = GetComponent<SpriteRenderer>();
-		animator = GetComponent<Animator>();
-        rb2d = GetComponent<Rigidbody2D>();
-
-        m_WhatIsGround = Physics2D.GetLayerCollisionMask(gameObject.layer);
-
-        m_mainGravityScale = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2) ;
-        rb2d.gravityScale = m_mainGravityScale / Physics2D.gravity.y;
-        maxJumpVelocity = Mathf.Abs(m_mainGravityScale) * timeToJumpApex;
-        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(m_mainGravityScale) * minJumpHeight);
     }
 
     private void Update()
 	{     
-        //      Velocity = Vector2.zero;
-        //Vector2 move = Vector2.zero;
-
-        //move.x = Input.GetAxis("Horizontal");
-
-        //if (Input.GetButtonDown("Jump") && grounded)
-        //{
-        //          rb2d.velocity = new Vector2(rb2d.velocity.x, maxJumpVelocity);
-        //          //Debug.Break();
-        //          Velocity.y = maxJumpVelocity;
-        //      }
-        //else if (Input.GetButtonUp("Jump"))
-        //{
-        //          if (rb2d.velocity.y > minJumpVelocity)
-        //          {
-        //              rb2d.velocity = new Vector2(rb2d.velocity.x, minJumpVelocity);
-        //              Velocity.y = minJumpVelocity;
-        //          }
-        //}
-
-        //      if (Input.GetButton("Jump") && rb2d.velocity.y < 0)
-        //          rb2d.velocity = new Vector2(rb2d.velocity.x, Mathf.SmoothDamp(rb2d.velocity.y, maxVericalGlideSpeed, ref velocityYSmoothing, 0.1f));
-        //      //rb2d.velocity = new Vector2(rb2d.velocity.x, Vector2.ClampMagnitude(rb2d.velocity, maxVericalSpeed).y);
-        //      //if (Input.GetButton("Jump") && rb2d.velocity.y < 0)
-        //      //    rb2d.gravityScale = m_glideGravityScale / Physics2D.gravity.y;
-        //      //else
-        //      //    rb2d.gravityScale = m_mainGravityScale / Physics2D.gravity.y;
-
-        //      bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-        //if (flipSprite)
-        //{
-        //	spriteRenderer.flipX = !spriteRenderer.flipX;
-        //}
-
-        //animator.SetBool("grounded", grounded);
-        //      animator.SetFloat("velocityX", Mathf.Abs(rb2d.velocity.x) / maxHorizontalSpeed);
-
-        //      float m_targetVelocityX = move.x * maxHorizontalSpeed;
-
-        //      //Velocidad de movimiento lateral cuando el jugador esta...
-        //      if (Input.GetKey(KeyCode.LeftControl) && grounded)              //...agachado.
-        //          Velocity.x = Mathf.SmoothDamp(rb2d.velocity.x, m_targetVelocityX * CrouchSpeed, ref velocityXSmoothing, accelerationTimeGrounded);
-
-        //      else if (grounded)                         //...en el suelo.
-        //          Velocity.x = Mathf.SmoothDamp(rb2d.velocity.x, m_targetVelocityX, ref velocityXSmoothing, accelerationTimeGrounded);
-
-        //      else if (Input.GetButton("Jump") && rb2d.velocity.y < 0)    //...con el paraguas.
-        //          Velocity.x = Mathf.SmoothDamp(rb2d.velocity.x, m_targetVelocityX, ref velocityXSmoothing, accelerationTimeGrounded);
-
-        //      else if (!grounded)                                         //...callendo.
-        //          Velocity.x = Mathf.SmoothDamp(rb2d.velocity.x, m_targetVelocityX, ref velocityXSmoothing, accelerationTimeAirborne);
     }
+
 
     private void FixedUpdate()
     {
-        rb2d.velocity = new Vector2(Velocity.x, rb2d.velocity.y);
-        Grounded();
+		Grounded();
 
-        Velocity = Vector2.zero;
-        Vector2 move = Vector2.zero;
+		Velocity = Vector2.zero;
+		Vector2 move = Vector2.zero;
 
-        if (!Death)
+		if (!Death) {
+			switch (PlayerMode) {
+			case MovementMode.OnGround:
+				UpdateGround ();
+				break;
+			case MovementMode.OnAir:
+				UpdateAir ();
+				break;
+			case MovementMode.OnClimbing:
+				UpdateClimb ();
+				break;
+			}
+		}
+		/*
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite)
         {
-            move.x = Input.GetAxis("Horizontal");
-
-            if (Input.GetButtonDown("Jump") && grounded)
-            {
-                rb2d.velocity = new Vector2(rb2d.velocity.x, maxJumpVelocity);
-                //Debug.Break();
-                Velocity.y = maxJumpVelocity;
-            }
-            else if (Input.GetButtonUp("Jump"))
-            {
-                if (rb2d.velocity.y > minJumpVelocity)
-                {
-                    rb2d.velocity = new Vector2(rb2d.velocity.x, minJumpVelocity);
-                    Velocity.y = minJumpVelocity;
-                }
-            }
-
-            if (Input.GetButton("Jump") && rb2d.velocity.y < 0)
-                rb2d.velocity = new Vector2(rb2d.velocity.x, Mathf.SmoothDamp(rb2d.velocity.y, maxVericalGlideSpeed, ref velocityYSmoothing, 0.1f));
-
-            bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-            if (flipSprite)
-            {
-                spriteRenderer.flipX = !spriteRenderer.flipX;
-            }
-
-            float m_targetVelocityX = move.x * maxHorizontalSpeed;
-
-            //Velocidad de movimiento lateral cuando el jugador esta...
-            if (Input.GetKey(KeyCode.LeftControl) && grounded)              //...agachado.
-                Velocity.x = Mathf.SmoothDamp(rb2d.velocity.x, m_targetVelocityX * CrouchSpeed, ref velocityXSmoothing, accelerationTimeGrounded);
-
-            else if (grounded)                         //...en el suelo.
-                Velocity.x = Mathf.SmoothDamp(rb2d.velocity.x, m_targetVelocityX, ref velocityXSmoothing, accelerationTimeGrounded);
-
-            else if (Input.GetButton("Jump") && rb2d.velocity.y < 0)    //...con el paraguas.
-                Velocity.x = Mathf.SmoothDamp(rb2d.velocity.x, m_targetVelocityX, ref velocityXSmoothing, accelerationTimeGrounded);
-
-            else if (!grounded)                                         //...callendo.
-                Velocity.x = Mathf.SmoothDamp(rb2d.velocity.x, m_targetVelocityX, ref velocityXSmoothing, accelerationTimeAirborne);
+            spriteRenderer.flipX = !spriteRenderer.flipX;
         }
 
         animator.SetBool("grounded", grounded);
-        animator.SetFloat("velocityX", Mathf.Abs(rb2d.velocity.x) / maxHorizontalSpeed);
+        animator.SetFloat("velocityX", Mathf.Abs(rb2d.velocity.x) / maxHorizontalSpeed);*/
     }
+
+	//Velocity Variables ground
+	[SerializeField]
+	float groundAccel;
+	[SerializeField]
+	float groundMaxSpeed;
+	[SerializeField]
+	float crouchedMaxSpeed;
+	[SerializeField]
+	float groundFriction;
+	[SerializeField]
+	float gravityOnGround;
+	[SerializeField]
+	float gravityOnAir;
+	[SerializeField]
+	float airAccel;
+	[SerializeField]
+	float airFriction;
+	[SerializeField]
+
+	bool jumpPressed;
+	bool jumpPressedBefore;
+
+	private void UpdateGround(){
+		//Setear velocidad máxima
+		float speedToUse = groundMaxSpeed;
+		if(Input.GetKey(KeyCode.LeftControl)){
+			speedToUse = crouchedMaxSpeed;
+		}
+		float xSpeed = 0;
+		if (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0.1f)
+			//Apply acceleration
+			xSpeed = rb2d.velocity.x + Input.GetAxis ("Horizontal") * groundAccel * Time.deltaTime;
+		else
+			//Apply Friction
+			xSpeed = rb2d.velocity.x * (1 - groundFriction * Time.deltaTime);
+		xSpeed = Mathf.Clamp(xSpeed, - speedToUse ,speedToUse );
+		float ySpeed = rb2d.velocity.y + gravityOnGround * Time.deltaTime;
+
+		//Chequear Salto
+		jumpPressed = Input.GetButton("Jump");
+		bool jumpDown = jumpPressed && !jumpPressedBefore;
+		jumpPressedBefore = jumpPressed;
+
+		if (jumpDown && grounded)
+		{
+			ySpeed = maxJumpVelocity;
+		}
+
+		rb2d.velocity = xSpeed*Vector2.right + ySpeed*Vector2.up;
+
+		if (!grounded)
+			PlayerMode = MovementMode.OnAir;
+
+		if(Mathf.Abs(Input.GetAxis ("Vertical")) > 0.1f && grabbedTransform != null){
+			PlayerMode = MovementMode.OnClimbing;
+		}
+	}
+
+	private void UpdateAir(){
+		//Setear velocidad máxima
+		float speedToUse = groundMaxSpeed;
+		float xSpeed = 0;
+		if (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0.1f)
+			//Apply acceleration
+			xSpeed = rb2d.velocity.x + Input.GetAxis ("Horizontal") * airAccel * Time.deltaTime;
+		else {
+			//Apply Friction
+			xSpeed = rb2d.velocity.x * (1 - airFriction * Time.deltaTime);
+		}
+		xSpeed = Mathf.Clamp(xSpeed, - speedToUse ,speedToUse );
+		Debug.Log (xSpeed);
+
+		float gravityToUse = gravityOnGround;
+		if(rb2d.velocity.y > 0)
+			gravityToUse = gravityOnAir;
+		float ySpeed = rb2d.velocity.y + gravityToUse * Time.deltaTime;
+
+		//Chequear Salto
+		jumpPressed = Input.GetButton("Jump");
+		jumpPressedBefore = jumpPressed;
+		if (!jumpPressed && rb2d.velocity.y > minJumpVelocity)
+		{
+			ySpeed = minJumpVelocity;
+		}
+
+		rb2d.velocity = xSpeed*Vector2.right + ySpeed*Vector2.up;
+
+		if (grounded)
+			PlayerMode = MovementMode.OnGround;
+
+		if(Mathf.Abs(Input.GetAxis ("Vertical")) > 0.1f && grabbedTransform != null){
+			PlayerMode = MovementMode.OnClimbing;
+		}
+	}
+
+	Scr_ObjetoTrepar grabbedTransform;
+	[SerializeField]
+	float grabXInterpolation;
+	[SerializeField]
+	float climbSpeed;
+
+	private void UpdateClimb(){
+
+		jumpPressed = Input.GetButton("Jump");
+		bool jumpDown = jumpPressed && !jumpPressedBefore;
+		jumpPressedBefore = jumpPressed;
+		if (jumpDown || grabbedTransform == null) {
+			rb2d.velocity = maxJumpVelocity * Vector2.up;
+			PlayerMode = MovementMode.OnAir;
+		}else if(transform.position.y >= grabbedTransform.upPoint.position.y || transform.position.y <= grabbedTransform.downPoint.position.y || grounded && Input.GetAxis ("Vertical") < 0){
+			PlayerMode = MovementMode.OnGround;
+		}else{
+			Vector2 climb = Input.GetAxis ("Vertical") * climbSpeed * Vector2.up;
+			rb2d.velocity = climb;
+			transform.position =  new Vector3( Mathf.Lerp (rb2d.position.x, grabbedTransform.transform.position.x, Time.deltaTime * grabXInterpolation) , rb2d.position.y, transform.position.z);
+		}
+	}
+	[SerializeField]
+	Collider2D groundChecker;
 
     private void Grounded()
     {
-        grounded = false;
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, k_GroundedRadius, m_WhatIsGround);
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            if (colliders[i].gameObject != gameObject)
-                grounded = true;
-        }
+		grounded = groundChecker.IsTouchingLayers(m_WhatIsGround);
     }
+
+	public void OnClimb (Scr_ObjetoTrepar stair, bool enter){
+		if (enter) {
+			grabbedTransform = stair;
+		} else if (grabbedTransform == stair) {
+			grabbedTransform = null;
+		}	
+	}
 
     private void OnDrawGizmosSelected()
     {
