961a1270345b7916e27678044a5009bbe778fcf3
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-05-19 08:54:46 +0200

    C4

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -87,7 +87,7 @@ public class Player : MonoBehaviour
 		}
         anim.SetBool("grounded", grounded);
         anim.SetBool("jumpPressed", jumpPressed);
-        anim.SetFloat("velocityX", groundMaxSpeed);
+        anim.SetFloat("velocityX", rb2d.velocity.x);
     }
 
 	private void UpdateGround(){
