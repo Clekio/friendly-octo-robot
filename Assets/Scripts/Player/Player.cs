88ccfaa92533f52130f0e9f5e4ba41e9f5f933c3
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-05-20 15:57:49 +0200

    ANIMACIONES

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -67,6 +67,7 @@ public class Player : MonoBehaviour
     Animator anim;
 
     bool crouch = false;
+    bool planeo = false;
 
     private void FixedUpdate()
     {
@@ -169,7 +170,16 @@ public class Player : MonoBehaviour
 		}
 
         if (jumpPressed && rb2d.velocity.y < 0)
+        {
             ySpeed = Mathf.SmoothDamp(rb2d.velocity.y, maxVericalGlideSpeed, ref velocityYSmoothing, .1f);
+            planeo = true;
+            anim.SetBool("planeando", planeo);
+        }
+        else
+        {
+            planeo = false;
+            anim.SetBool("planeando", planeo);
+        }
 
         rb2d.velocity = xSpeed*Vector2.right + ySpeed*Vector2.up;
 
