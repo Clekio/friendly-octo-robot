82cbe1e5907f6d1dbece75682dfed359778ce55c
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-05-21 19:38:12 +0200

    456

---
+++ Assets/Scripts/Puzles/Scr_TriggerRaicesVuelta1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_TriggerRaicesVuelta1 : MonoBehaviour
+{
+    public bool trigger1 = false;
+    public static bool trigger2 = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        trigger1 = true;
+    }
+
+    void Update ()
+    {
+        if (trigger1 == true)
+        {
+            gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0, -30);
+            trigger2 = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Puzles/Scr_TriggerRaicesVuelta2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_TriggerRaicesVuelta2 : MonoBehaviour
+{
+    [SerializeField]
+    Animator raicesVuelta;
+
+    bool trigger2 = false;
+
+    void Start()
+    {
+        raicesVuelta = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        trigger2 = Scr_TriggerRaicesVuelta1.trigger2;
+        raicesVuelta.SetBool("espinasDown", trigger2);
+    }
+}
