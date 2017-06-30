228fb6f2c6f8f55b6e435206fcab87fe6a058d4a
porejemplo <ejemplo696@gmail.com>
2017-07-01 00:54:15 +0200

    Magia
    Magia, Gestos, ejemplo partículas, bullet time y shader escala de grises

--- Assets/Scripts/Runas/Runas.cs
+++ Assets/Scripts/Runas/Runas.cs
@@ -2,83 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Runas : RunasRecognizer {
+public class Runas : recognizerRunas
+{
+    private Vector3 thisDelta;
+    private Vector3 LastDelta;      //Delta del frame anterior.
 
+    private Vector3 mousePosition;
     private Vector3 LastMousePos;   //Posicion del mouse en el frame anterior.
-    private Vector3 LastDelta;    //Delta del frame anterior.
-
-    RunasTemplate m_Templates;
-
+    
     List<Vector2> m_pointList;
-    List<Vector2> m_deltaList;
 
-    [Header("Line Renderer")]
-    public GameObject linePrefab;
-    Line activeLine;
-    private GameObject lineGO;
+    [Header("Visual")]
+    public GameObject particlePrefab;
 
     [Header("Magic")]
     public float MagicTimeToDestroy;
 
-    public GameObject waterBall;
-    public GameObject waterBubble;
-    public GameObject viento;
-    public GameObject remolino;
+    public GameObject water;
+    public GameObject thunder;
+    public GameObject wind;
+    public GameObject swirl;
 
     // Use this for initialization
     void Start ()
     {
-        m_Templates = new RunasTemplate();
-
-        // Decir si se han cargado las Templates y cuantas son.
-        Debug.Log("Templates Loaded: " + RunasTemplate.TemplateRunasList.Count);
-
-        Debug.DrawLine(new Vector3(200, 200, 200), Vector3.zero, Color.green, 2, false);
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 thisDelta = (Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z)) - LastMousePos) / Time.deltaTime;
-
+        
         if (Input.GetMouseButtonDown(0))
         {
             //Resetear el los valores de la runa.
             m_pointList = new List<Vector2>();
 
-            m_deltaList = new List<Vector2>();
-
-            //Se crea una nueva linea y se coje la referencia al script
-            lineGO = Instantiate(linePrefab);
-            activeLine = lineGO.GetComponent<Line>();
+            //se crean las particulas
+            Instantiate(particlePrefab);
         }
 
         if (Input.GetMouseButton(0))
         {
             //Guardar las posiciones en las que se dibuja
-            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z));
+            mousePosition = Input.mousePosition; //Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z));
 
-            m_pointList.Add(p);
-            m_deltaList.Add(thisDelta);
+            thisDelta = (mousePosition - LastMousePos) / Time.deltaTime;
 
-            if (activeLine != null)
-                activeLine.UpdateLine(p);
+            if ((thisDelta - LastDelta).magnitude < 0.05f)
+                m_pointList.Add(mousePosition);
+
+            LastDelta = thisDelta;
+            LastMousePos = mousePosition;
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             //Reconocer la Runa
-            StartRecognizer(m_pointList, m_deltaList);
+            StartRecognizer(m_pointList);//, m_deltaList);
 
             //Crear la magia
             SpawnMagic(m_magicName, m_magicPosition, m_magicAngle, m_magicScale);
-
-            //Finalizar la interaccion con la linea.
-            activeLine = null;
-            Destroy(lineGO, MagicTimeToDestroy);
         }
-
-        LastDelta = thisDelta;
-        LastMousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z));
     }
 
     public void SpawnMagic(string name, Vector2 position, float angle, Vector2 scale)
@@ -87,12 +71,49 @@ public class Runas : RunasRecognizer {
 
         if (name == "wind")
         {
-            effect = Instantiate(viento, new Vector3(position.x, position.y, 0), Quaternion.identity) as GameObject;
+            effect = Instantiate(wind, new Vector3(position.x, position.y, 0), Quaternion.identity) as GameObject;
             effect.transform.localScale = scale;
             effect.GetComponent<AreaEffector2D>().forceAngle = angle;
             Destroy(effect, MagicTimeToDestroy);
         }
+
+        else if (name == "swirl")
+        {
+            effect = Instantiate(swirl, new Vector3(position.x, position.y, 0), Quaternion.identity) as GameObject;
+            effect.transform.localScale = scale;
+            Destroy(effect, MagicTimeToDestroy);
+        }
+
+        else if (name == "water")
+        {
+            effect = Instantiate(water, new Vector3(position.x, position.y, 0), Quaternion.Euler(90,90,0)) as GameObject;
+            //effect.transform.localScale = scale;
+            Destroy(effect, 11.0f);
+        }
+
+        else if (name == "thunder")
+        {
+            effect = Instantiate(thunder, new Vector3(position.x, position.y, 0), Quaternion.identity) as GameObject;
+            //effect.transform.localScale = scale;
+            Destroy(effect, 3.0f);
+        }
     }
 
+//#if UNITY_EDITOR
+    private Rect windowRect = new Rect(3, 3, 100, 45);
 
+    void OnGUI()
+    {
+        windowRect = GUI.Window("RunasDebug".GetHashCode(), windowRect, DrawWindowContents, "Magia Name");
+    }
+
+    void DrawWindowContents(int windowId)
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("   " + m_magicName);
+        GUILayout.EndHorizontal();
+
+        GUI.DragWindow();
+    }
+//#endif
 }
