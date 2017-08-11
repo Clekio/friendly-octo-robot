27a5d8603912f56cb1ba29bf42806d2217ce45b8
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-08-10 21:57:09 -0700

    Checkpoint system

---
+++ Assets/Scripts/Checkpoint System/Scr_KillPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_KillPlayer : MonoBehaviour
+{
+    public Scr_LevelManager levelManager;
+
+    private void Start()
+    {
+        levelManager = FindObjectOfType<Scr_LevelManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Aura")
+        {
+            levelManager.RespawnPlayer();
+        }
+    }
+}
