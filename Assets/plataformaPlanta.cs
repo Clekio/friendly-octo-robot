2254622200d770002a8c9506379cfe468914aecb
porejemplo <ejemplo696@gmail.com>
2017-09-06 19:46:49 +0200

    plantas 1

---
+++ Assets/plataformaPlanta.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataformaPlanta : MonoBehaviour {
+
+	bool open = false;
+
+	public float retraso = 4;
+
+	public Animator anim;
+
+	private void OnParticleCollision(GameObject other)
+	{
+		if (!open) 
+		{
+			open = true;
+			anim.SetBool ("mojado", open);
+			Debug.Log ("heyyyyyy");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	private IEnumerator DelayTimerSecado (float delayLength){
+		yield return new WaitForSeconds (delayLength);
+		Debug.Log ("tiempo completo");
+
+	}
+}
