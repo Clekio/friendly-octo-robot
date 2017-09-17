64268168e34185b384288ed721485b38218e4bfc
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-09-17 15:44:29 -0700

    Done

---
+++ Assets/Scripts/Scene management/Scr_BotonDelPanico.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Scr_BotonDelPanico : MonoBehaviour
+{
+    GameObject Aura;
+
+    Vector3 checkpoint;
+    string checkpointName;
+
+    private void Start()
+    {
+        Aura = GameObject.Find("Aura");
+
+        DontDestroyOnLoad(Aura);
+    }
+
+    private void Update()
+    {
+        checkpoint = Scr_PlayerCheckpoint.checkpoint;
+        checkpointName = Scr_PlayerCheckpoint.checkpointName;
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        SceneManager.LoadScene("Scn_PruebaVictor", LoadSceneMode.Single);
+
+        Aura.transform.position = checkpoint;
+    }
+}
