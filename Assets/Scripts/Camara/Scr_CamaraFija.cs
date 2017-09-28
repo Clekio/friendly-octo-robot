668e58a24efc326fae96d6ec9c26461cd078a1a1
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-09-28 00:00:59 -0700

    1

---
+++ Assets/Scripts/Camara/Scr_CamaraFija.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_CamaraFija : MonoBehaviour
+{
+    GameObject camaraMovil;
+
+    private void Start()
+    {
+        camaraMovil = GameObject.Find("Main Camera");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            camaraMovil.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        camaraMovil.SetActive(true);
+    }
+}
