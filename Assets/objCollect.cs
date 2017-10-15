b83fb1aaa51aed7bda4298208436f1fbad775462
porejemplo <ejemplo696@gmail.com>
2017-10-15 13:18:31 +0200

    Puerta
    La puerta cambia de color cuando coges los triangulos

---
+++ Assets/objCollect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class objCollect : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            gameObject.SetActive(false);
+    }
+}
---
+++ Assets/objPuerta.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objPuerta : MonoBehaviour {
+
+    public List<GameObject> triangulos = new List<GameObject>();
+
+    public SpriteRenderer sRender;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && areTriangles())
+        {
+            sRender.color = Colors.Magenta;
+        }
+
+    }
+
+    private bool areTriangles()
+    {
+        bool b = true;
+
+        foreach (GameObject g in triangulos)
+        {
+            if (g.activeSelf)
+            {
+                b = false;
+            }
+        }
+
+        return b;
+    }
+}
