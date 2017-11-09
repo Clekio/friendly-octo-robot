3c12e5675e781a90ebc7ff6d3efd73f1f0e35832
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-11-09 11:26:12 -0800

    Cambios 1
    Creación script y pruebas

---
+++ Assets/Scripts/_Scripts Prólogo/Balancín/Scr_Balancin.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Balancin : MonoBehaviour
+{
+    [SerializeField]
+    int tiempoReset;
+
+    [SerializeField]
+    float rotationSpeed;
+
+    Transform target;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        target = transform.parent;
+
+        rb = gameObject.GetComponentInParent<Rigidbody2D>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rb.SetActive(false);
+
+            Invoke("ResetBalancin", tiempoReset);
+        }
+    }
+
+    void ResetBalancin()
+    {
+        transform.parent.rotation = Quaternion.Lerp(transform.parent.rotation, target.rotation, rotationSpeed);
+
+        if (transform.parent.rotation == target.rotation)
+        {
+
+        }
+    }
+}
