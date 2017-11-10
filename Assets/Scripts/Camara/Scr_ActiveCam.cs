c93a20f2f3da9f52a24bb8b19ecb300cd3c839d3
Clekio <alvaro.ortuno.m@gmail.com>
2017-11-10 19:03:31 +0100

    Hotspots y camara
    23

---
+++ Assets/Scripts/Camara/Scr_ActiveCam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_ActiveCam : MonoBehaviour {
+
+    public GameObject camera1;
+
+    public GameObject camera2;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            camera1.SetActive(true);
+            camera2.SetActive(false);
+        }
+    }
+
+}
