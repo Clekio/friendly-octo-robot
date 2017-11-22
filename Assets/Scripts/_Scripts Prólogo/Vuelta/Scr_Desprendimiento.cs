c892aa8fa31bb0d3fc9207fca93b87ec571aa009
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-11-21 21:29:38 -0800

    Scrip desprendimiento terminado y funcionando

---
+++ Assets/Scripts/_Scripts Prólogo/Vuelta/Scr_Desprendimiento.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Desprendimiento : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Find("Suelo 1 (3) - Vuelta").GetComponent<MeshRenderer>().enabled = true;
+            GameObject.Find("Suelo 1 (3) - Vuelta").GetComponent<BoxCollider2D>().enabled = true;
+
+            GameObject.Find("Suelo 1 (6) - Vuelta").GetComponent<MeshRenderer>().enabled = true;
+            GameObject.Find("Suelo 1 (6) - Vuelta").GetComponent<BoxCollider2D>().enabled = true;
+
+            GameObject.Find("Suelo 1 (7) - Vuelta").GetComponent<MeshRenderer>().enabled = true;
+            GameObject.Find("Suelo 1 (7) - Vuelta").GetComponent<BoxCollider2D>().enabled = true;
+
+            GameObject.Find("Suelo 1 (18) - Vuelta").GetComponent<MeshRenderer>().enabled = true;
+            GameObject.Find("Suelo 1 (18) - Vuelta").GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}
