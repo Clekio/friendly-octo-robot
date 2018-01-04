a98f59be2ea54d08325dfed0c249351e46c7015d
fernando <ejemplo696@gmail.com>
2018-01-04 14:45:57 +0100

    Borrar magias

--- Assets/Scripts/Runas/ListasDeMagias/MagiaList.cs
+++ Assets/Scripts/Runas/ListasDeMagias/MagiaList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Magia List", menuName = "Magia List")]
 public class MagiaList : ScriptableObject
 {
     public List<Magia> magias;
