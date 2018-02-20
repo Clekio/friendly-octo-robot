40a21eb7c7fafac3fb5a86a6e12056747cdaa19b
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-02-20 02:10:09 +0100

    Scr_ResetPosition añadido
    Scr_ResetPosition añadido

---
+++ Assets/Scripts/_Scripts Prólogo/Pull&Push object/Scr_ResetPosition.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_ResetPosition : MonoBehaviour
+{
+    [SerializeField] int checkDistance; // Distancia en el eje vertical límite para el ojbeto antes de retornar a su posición inicial
+    [SerializeField] float timeToReset; // Tiempo que está el objeto incativo antes de volver a su posición inical
+    Vector3 startPosition;
+    float timer;
+    bool timerRunning;
+    BoxController boxController;
+
+    private void Awake()
+    {
+        startPosition = GetComponent<Transform>().position;
+    }
+
+    private void Update()
+    {
+        if (timerRunning == true)
+            timer -= Time.deltaTime;
+
+        if (boxController.moving == true)
+        {
+            timerRunning = false;
+            ResetTimer(timeToReset);
+        }
+
+        if (startPosition.y < (startPosition.y - checkDistance))
+        {
+            timerRunning = true;
+        }
+
+        if (timer <= 0)
+            ResetPosition();
+    }
+
+    public void ResetPosition()
+    {
+        gameObject.transform.position = startPosition;
+    }
+
+    public void ResetTimer(float newTime)
+    {
+        timer = newTime;
+    }
+}
