920b1f3a071e78e175862c714ea7f8b52109f496
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-02-26 20:04:14 -0800

    Animaciones principales y setup

---
+++ Assets/Scripts/_Scripts Menús/Scr_ControladorMenuPpal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_ControladorMenuPpal : MonoBehaviour
+{
+    Animator logo;
+    Animator textoStart;
+    Animator jugar;
+    Animator controles;
+
+    bool intro = false;
+
+    private void Awake()
+    {
+        logo = GameObject.Find("Logo").GetComponent<Animator>();
+        textoStart = GameObject.Find("TextoStart").GetComponent<Animator>();
+        jugar = GameObject.Find("Jugar").GetComponent<Animator>();
+        controles = GameObject.Find("Controles").GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button7))
+        {
+            logo.SetBool("On", true);
+            textoStart.SetBool("On", true);
+            jugar.SetBool("On", true);
+            controles.SetBool("On", true);
+        }
+    }
+}
