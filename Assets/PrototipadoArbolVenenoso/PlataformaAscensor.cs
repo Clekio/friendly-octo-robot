7cce22472910720ace9647be8eed680cc336c4ff
SrAlexen <alejandro.lpz.munoz@gmail.com>
2018-03-19 02:57:00 +0100

    Prueba puzle arbol venenoso

---
+++ Assets/PrototipadoArbolVenenoso/PlataformaAscensor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaAscensor : MonoBehaviour {
+
+	public Animator anim;
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Pull&Push")
+			anim.SetBool ("Corriente", false);
+	}
+
+	void OnTriggerExit(Collider other){
+
+			Debug.Log (3);
+			anim.SetBool ("Corriente", true);
+	}
+}
